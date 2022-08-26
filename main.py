68e4a89e17d35d3bc712714d752ee59d1e224ada
ark10806 <ark10806@gmail.com>
2022-08-27 03:49:23 +0900

    [Add] multiple gpu

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ if opt.batch_size == -1:
   batch_list = [(2)**x for x in range(int(log(opt.min_batch_size,2)), int(log(opt.max_batch_size,2))+1)]
 else:
   batch_list = [opt.batch_size]
-  
+
 for batch_size in batch_list:
-  print(batch_size)
   dataloader = load_data('mnist', batch_size=batch_size, num_workers=opt.n_workers)
-  model = ResNet50(dataloader, device, opt.n_gpu, verbose=False)
+  print(batch_size)
+  model = ResNet50(dataloader, device, opt.n_gpu, verbose=True)
   tps_res = model.train(epochs=opt.epochs)
   output.append( (batch_size, tps_res['train']) )
 
