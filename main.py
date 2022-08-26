cd2c36d92332076c426081de0257ef0a58a7f48e
ark10806 <ark10806@gmail.com>
2022-08-27 02:56:43 +0900

    [Add] main

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 for batch_size in [16,32,64,128,256,512,1024,2048]:
   dataloader = load_data('mnist', batch_size=batch_size, num_workers=opt.n_workers)
   model = ResNet50(dataloader, device, verbose=False)
-  print(model.train(epochs=opt.epochs))
+  print(f'{batch_size}:\t {model.train(epochs=opt.epochs)}')
