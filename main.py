c9640f00f487d1788219d03697f88789b856b938
ark10806 <ark10806@gmail.com>
2022-08-27 03:44:12 +0900

    [Add] multiple gpu

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ opt = args.init()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 output = []
-for batch_size in [(2)**x for x in range(int(log(opt.min_batch_size,2)), int(log(opt.max_batch_size,2))+1)]:
+if opt.batch_size == -1:
+  batch_list = [(2)**x for x in range(int(log(opt.min_batch_size,2)), int(log(opt.max_batch_size,2))+1)]
+else:
+  batch_list = [opt.batch_size]
+  
+for batch_size in batch_list:
   print(batch_size)
   dataloader = load_data('mnist', batch_size=batch_size, num_workers=opt.n_workers)
   model = ResNet50(dataloader, device, opt.n_gpu, verbose=False)
--- pytorch_models/tools/args.py
+++ pytorch_models/tools/args.py
@@ -5,7 +5,7 @@ def init():
   parser.add_argument('--min_batch_size', type=int, default=16)
   parser.add_argument('--max_batch_size', type=int, default=2048)
   parser.add_argument('--epochs', default=3, type=int)
-  parser.add_argument('--batch_size', default=512, type=int)
+  parser.add_argument('--batch_size', default=-1, type=int)
   parser.add_argument('--n_workers', default=4, type=int)
   parser.add_argument('--n_gpu', default=1, type=int)
   return parser.parse_args()
