369e1d4304c06e2ee75d8d61319e25c4978b1737
ark10806 <ark10806@gmail.com>
2022-08-27 01:56:07 +0900

    [code]: argparser epochs

--- pytorch_models/resnet50.py
+++ pytorch_models/resnet50.py
@@ -73,4 +73,4 @@ if __name__ == '__main__':
   opt = args.init()
   device = 'cuda' if torch.cuda.is_available() else 'cpu'
   model = ResNet50(dataloader.load_data('mnist', batch_size=opt.batch_size, num_workers=opt.n_workers), device, verbose=True)
-  print(model.train(epochs=10))
+  print(model.train(epochs=opt.epochs))
