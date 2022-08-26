7adb7162d6a13a974fbf7108a9d4aaf92144525d
ark10806 <ark10806@gmail.com>
2022-08-26 21:44:53 +0900

    [fixed]: Dataset class

--- pytorch-models/resnet50.py
+++ pytorch-models/resnet50.py
@@ -1,7 +1,7 @@
 import timm
 from dataloader import Dataset
 
-train, val = Dataset.load('mnist')
+train, val = Dataset().load('mnist')
 
 model = timm.create_model(model_name='resnet50', pretrained=True)
 print(dir(model))
