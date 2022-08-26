eddca4e033cab90cbf68614d75193e2fe16c785c
ark10806 <ark10806@gmail.com>
2022-08-26 23:53:21 +0900

    [Test] ResNet50-Mnist

--- pytorch-models/resnet50.py
+++ pytorch-models/resnet50.py
@@ -1,9 +1,10 @@
+import sys
 import timm
 import torch
 from tqdm import tqdm
 import torch.nn as nn
 import dataloader
-
+import init
 
 class ResNet50:
   def __init__(self, dataloader, device):
@@ -14,19 +15,15 @@ class ResNet50:
 
   def init_model(self):
     self.model = timm.create_model('resnet50', pretrained=True, in_chans=1, num_classes=10)
-    print(self.model)
-    # self.model.conv1 = nn.Conv2d(1, 64, kernel_size=(7,7), stride=(2,2), padding=(3,3), bias=False)
-    # self.model.bn1 = nn.BatchNorm2d(28)
-    self.model.fc = nn.Linear(in_features=2048, out_features=10)
 
   def train(self, epochs):
     for epoch in range(epochs):
       print(f'epoch: {epoch}')
-      self.train_one_epoch()
+      # self.train_one_epoch()
       self.eval()
 
   def train_one_epoch(self):
-    for image, label in tqdm(self.dataloader['train']):
+    for image, label in tqdm(self.dataloader['train'], desc='train'):
       image = image.to(self.device).float()
       label = label.to(self.device)
       
@@ -36,17 +33,18 @@ class ResNet50:
 
   def eval(self):
     with torch.no_grad():
-      for image, label in tqdm(self.dataloader['valid']):
+      for image, label in tqdm(self.dataloader['valid'], desc='eval'):
         image = image.to(self.device)
         label = label.to(self.device)
 
         pred = self.model(image)
         loss = self.loss_fn(pred, label)
         correct = torch.argmax(pred, 1) == label
-        print(f'val: {correct}, {loss}')
+      print(f'val: {torch.sum(correct)/len(correct)*100:.2f}%, {loss:.4f}')
 
 if __name__ == '__main__':
+  opt = init.init()
   device = 'cuda' if torch.cuda.is_available() else 'cpu'
-  model = ResNet50(dataloader.load_data('mnist', 16, 4), 'cpu')
+  model = ResNet50(dataloader.load_data('mnist', batch_size=opt.batch_size, num_workers=opt.n_workers), device)
   print(device)
   model.train(epochs=10)
