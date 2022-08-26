05749b28fc315b5dda174f28bd06a13b4c058fbc
ark10806 <ark10806@gmail.com>
2022-08-27 04:04:19 +0900

    [Arg] n_gpu -1 for Max gpus

--- pytorch_models/tools/args.py
+++ pytorch_models/tools/args.py
@@ -2,8 +2,8 @@ import argparse
 
 def init():
   parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument('--min_batch_size', type=int, default=512)
-  parser.add_argument('--max_batch_size', type=int, default=2048)
+  parser.add_argument('--min_batch_size', type=int, default=256)
+  parser.add_argument('--max_batch_size', type=int, default=4096)
   parser.add_argument('--epochs', default=1, type=int)
   parser.add_argument('--batch_size', default=-1, type=int)
   parser.add_argument('--n_workers', default=4, type=int)
