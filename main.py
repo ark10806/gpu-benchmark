feffe1feccb52f03ae21bc6fabb561676a4aee28
ark10806 <ark10806@gmail.com>
2022-08-27 03:15:43 +0900

    [Add] multiple batch_size

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import torch
+from math import log
+
 from pytorch_models.tools import args
 from pytorch_models.resnet50 import ResNet50
 from pytorch_models.dataloader import load_data
@@ -7,7 +9,7 @@ from pytorch_models.dataloader import load_data
 opt = args.init()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-for batch_size in [16,32,64,128,256,512,1024,2048]:
+for batch_size in [(2)**x for x in range(int(log(opt.min_batch_size,2)), int(log(opt.max_batchsize,2))+1)]:
   dataloader = load_data('mnist', batch_size=batch_size, num_workers=opt.n_workers)
   model = ResNet50(dataloader, device, verbose=False)
   print(f'{batch_size}:\t {model.train(epochs=opt.epochs)}')
--- pytorch_models/tools/args.py
+++ pytorch_models/tools/args.py
@@ -2,6 +2,8 @@ import argparse
 
 def init():
   parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('--min_batch_size', type=int, default=16)
+  parser.add_argument('--max_batch_size', type=int, default=2048)
   parser.add_argument('--epochs', default=3, type=int)
   parser.add_argument('--batch_size', default=512, type=int)
   parser.add_argument('--n_workers', default=4, type=int)
