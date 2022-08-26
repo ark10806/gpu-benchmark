ff1da154807a49496e57d640c7e05054bcd53314
ark10806 <ark10806@gmail.com>
2022-08-27 01:50:34 +0900

    [rm]

--- pytorch-models/dataloader2.py
+++
@@ -1,9 +0,0 @@
-import torch
-from dataset import Dataset
-
-def __init__(self, dataset):
-    dataset = Dataset().load(dataset)
-    splits = ['train', 'test']
-    shuffle = {'train': True, 'test': False}
-
-return dataset['']
