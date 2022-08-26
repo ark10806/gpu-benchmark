3fa0b2896eb48f691f4a1e18c558a7e39d5f7449
ark10806 <ark10806@gmail.com>
2022-08-27 03:16:18 +0900

    [Add] multiple batch_size

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pytorch_models.dataloader import load_data
 opt = args.init()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-for batch_size in [(2)**x for x in range(int(log(opt.min_batch_size,2)), int(log(opt.max_batchsize,2))+1)]:
+for batch_size in [(2)**x for x in range(int(log(opt.min_batch_size,2)), int(log(opt.max_batch_size,2))+1)]:
   dataloader = load_data('mnist', batch_size=batch_size, num_workers=opt.n_workers)
   model = ResNet50(dataloader, device, verbose=False)
   print(f'{batch_size}:\t {model.train(epochs=opt.epochs)}')
