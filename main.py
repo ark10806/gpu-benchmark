680873d3a1e0fdbd2094952d1883fc384037e639
ark10806 <ark10806@gmail.com>
2022-08-27 02:53:35 +0900

    [Add] main

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import torch
 from pytorch_models.tools import args
 from pytorch_models.resnet50 import ResNet50
-import pytorch_models.dataloader as dataloader
+from pytorch_models.dataloader import load_data
 
 
 opt = args.init()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = ResNet50(dataloader.load_data('mnist', batch_size=opt.batch_size, num_workers=opt.n_workers), device, verbose=True)
-print(model.train(epochs=opt.epochs))
+
+for batch_size in [16,32,64,128,256,512,1024,2048]:
+  dataloader = load_data('mnist', batch_size=batch_size, num_workers=opt.n_workers)
+  model = ResNet50(dataloader, device, verbose=False)
+  print(model.train(epochs=opt.epochs))
